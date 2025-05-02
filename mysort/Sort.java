260e83649063f5abade1a8cf394f5967ce793b3f
Gene-3 <rhkd352@naver.com>
2025-05-02 14:20:30 +0900

    Add files via upload

---
+++ mysort/Sort.java
@@ -0,0 +1,24 @@
+package sort.mysort;
+
+import java.util.Arrays;
+
+public abstract class Sort {
+
+	int[] orgData;
+	int[] sortedData;
+	
+	public abstract void sort();
+	
+	public void setData(int[] dataList) {
+		this.orgData = dataList;
+		this.sortedData = dataList.clone(); // deep copy
+	}
+	
+	public String getOrgDate() {
+		return Arrays.toString(orgData);
+	}
+	
+	public String getSortedData() {
+		return Arrays.toString(sortedData);
+	}
+}
